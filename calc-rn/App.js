17ae563091d145d9391f17a5745b76b86f2da5e6
Yeon <yeonheejae@office.skhu.ac.kr>
2025-10-17 03:21:16 +0900

    3rd Assignment

--- calc-rn/App.js
+++ calc-rn/App.js
@@ -3,111 +3,131 @@ import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import Button, { ButtonTypes } from './Components/Button';
 import { useState } from 'react';
 
-function App() {
-  const [result,setResult] = useState(0);
+const Operator = {
+  CLEAR: 'C',
+  PLUS: '+',
+  MINUS: '-',
+  EQUAL: '=',
+}
 
-  const {width} = useWindowDimensions();
-  const ButtonWidth = width / 4;
+function App() {
+  const [result, setResult] = useState(0);
+  const { width } = useWindowDimensions();
+  const ButtonWidth = width / 4 - 1;
+  const [formula, setFormula] = useState([]);
+
+  const onPressNumber = (number) => {
+    const last = formula[formula.length - 1];
+    if (isNaN(last)) {
+      setResult(number);
+      setFormula((prev) => [...prev, number]);
+    } else {
+      const newNumber = (last ?? 0) * 10 + number;
+      setResult(newNumber);
+      setFormula((prev) => {
+        prev.pop()
+        return [...prev, newNumber];
+      })
+    }
+  }
+
+  const onPressOperator = (operator) => {
+    switch (operator) {
+      case Operator.CLEAR:
+        setFormula([]);
+        setResult(0);
+        return;
+      case Operator.EQUAL:
+        calculate();
+        return;
+      default:
+        {
+          const last = formula[formula.length - 1]
+          if ([Operator.PLUS, Operator.MINUS].includes(last)) {
+            setFormula((prev) => {
+              return [...prev, operator]
+            })
+          } else {
+            setFormula((prev) => [...prev, operator])
+          }
+        }
+    }
+  }
+
+  let calculate = () => {
+    let calculateNumber = 0;
+    let operator = '';
+    formula.forEach((value) => {
+      if ([Operator.PLUS, Operator.MINUS].includes(value)) {
+        operator = value;
+      } else {
+        if (operator === Operator.PLUS) {
+          calculateNumber = calculateNumber + value;
+        } else if (operator === Operator.MINUS) {
+          calculateNumber = calculateNumber - value;
+        } else {
+          calculateNumber = value;
+        }
+      }
+    })
+    setResult(calculateNumber);
+    setFormula([calculateNumber]);
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar style = 'light'/>
-      <View style = {styles.resultContainer}>
-        <Text style = {styles.text}>{result}</Text>
+      <StatusBar style='light' />
+      <View style={styles.resultContainer}>
+        <Text style={styles.text}>{result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</Text>
       </View>
-      <View style = {styles.buttonContainer}> 
+      <View style={styles.buttonContainer}>
         <View style={styles.leftPad}>
           <View style={styles.number}>
-            <Button
-              title="1"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num, key) => (
               <Button
-              title="2"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
+                key={key}
+                title={num}
+                buttonType={ButtonTypes.NUMBER}
+                buttonStyle={{ width: ButtonWidth, height: ButtonWidth, marginBottom: 1 }}
+                onPress={() => onPressNumber(num)}
               />
-              <Button
-              title="3"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="4"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="5"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="6"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="7"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="8"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
-              <Button
-              title="9"
-              onPress={()=>{}}
-              buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
+            ))}
           </View>
           <View style={styles.bottom}>
             <Button
-              title="0"
-              onPress={()=>{}}
+              title={"0"}
               buttonType={ButtonTypes.NUMBER}
-              buttonStyle={{width: ButtonWidth*2, height: ButtonWidth}}
-              />
-              <Button
-              title="="
-              onPress={()=>{}}
+              buttonStyle={{ width: ButtonWidth * 2, height: ButtonWidth, marginTop: 1 }}
+              onPress={() => onPressNumber(0)}
+            />
+            <Button
+              title={Operator.EQUAL}
               buttonType={ButtonTypes.OPERATOR}
-              buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-              />
+              buttonStyle={{ width: ButtonWidth, height: ButtonWidth, marginTop: 1 }}
+              onPress={() => onPressOperator(Operator.EQUAL)}
+            />
           </View>
         </View>
         <View style={styles.operator}>
           <Button
-            title="C"
-            onPress={()=>{}}
+            title={Operator.CLEAR}
             buttonType={ButtonTypes.OPERATOR}
-            buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-            />
-            <Button
-            title="-"
-            onPress={()=>{}}
+            buttonStyle={{ width: ButtonWidth, height: ButtonWidth, marginTop: 1 }}
+            onPress={() => onPressOperator(Operator.CLEAR)}
+          />
+          <Button
+            title={Operator.MINUS}
             buttonType={ButtonTypes.OPERATOR}
-            buttonStyle={{width: ButtonWidth, height: ButtonWidth}}
-            />
-            <Button
-            title="+"
-            onPress={()=>{}}
+            buttonStyle={{ width: ButtonWidth, height: ButtonWidth, marginTop: 1 }}
+            onPress={() => onPressOperator(Operator.MINUS)}
+          />
+          <Button
+            title={Operator.PLUS}
             buttonType={ButtonTypes.OPERATOR}
-            buttonStyle={{width: ButtonWidth, height: ButtonWidth*2}}
-            />
-          </View>
+            buttonStyle={{ width: ButtonWidth, height: ButtonWidth * 2, marginTop: 1 }}
+            onPress={() => onPressOperator(Operator.PLUS)}
+          />
+        </View>
       </View>
     </View>
   );
@@ -116,16 +136,13 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     alignItems: 'stretch',
-    justifyContent: 'stretch',
+    justifyContent: 'flex-end',
   },
   text: {
-    fontSize: 60,
-    fontWeight: '700',
-    color: '#ffffff',
-    paddingBottom: 30,
-    paddingRight: 30,
+    fontSize: 50,
+    padding: 20,
+    color: 'white',
     textAlign: 'right'
   },
   resultContainer: {
@@ -136,26 +153,24 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
   buttonContainer: {
-    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'space-evenly',
     flexDirection: 'row',
   },
   leftPad: {
-    flex: 3,
-    flexDirection: 'column',
+    width: '75%',
   },
   number: {
-    flex: 3,
     flexDirection: 'row',
     flexWrap: 'wrap-reverse',
+    justifyContent: 'space-evenly',
   },
   bottom: {
     flexDirection: 'row',
-    alignItems:'flex-end',
+    justifyContent: 'space-evenly',
   },
   operator: {
-    flex: 1,
-    flexDirection: 'column',  
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-start'
   }
 });
 
